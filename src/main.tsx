2fd37773ff828aa2cdb31471ed4ac3e072908bbf
djosika <daniel.j.osika@gmail.com>
2025-09-28 19:04:31 -0400

    Update main.tsx
    rage quitting and deleting test files lol

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./camo-test.css";
-
-console.log("✅ main.tsx is running");
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
